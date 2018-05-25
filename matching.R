ae6bb866ffe8ae209ac62020827c271bafe6396c
Alex Zvoleff <azvoleff@gmail.com>
2018-05-25 15:28:20 -0400

    Add additional matching covariates.

--- matching.R
+++ matching.R
@@ -5,25 +5,25 @@ library(foreach)
 library(gdalUtils)
 library(dplyr)
 library(geoMatch)
+library(ggplot2)
 
 data_folder <- 'C:/Users/azvol/Code/LandDegradation/WOCAT/matching/Data'
 
 ###############################################################################
 # Setup rasters
 
-in_files <- list.files(data_folder, 'wocat_covariates-0000000000',
+in_files <- list.files(data_folder, 'wocat_covariates_1km-',
                        full.names=TRUE)
 vrt_file <- tempfile(fileext='vrt')
 gdalbuildvrt(in_files, vrt_file)
 r <- stack(vrt_file)
-#names(r) <- c('lpd_te7cl_v2', 'te_acces', 'te_eleva')
-names(r) <- c('lpd_te7cl_v2', 'te_eleva', 'te_pop')
+names(r) <- c('lpd_te7cl_v2', 'te_lulc', 'te_eleva', 'te_slop',
+              'te_te_clima', 'te_ppt', 'te_acces', 'te_pop')
 
 ###############################################################################
 # Pull control points from rasters
-
 set.seed(932)
-TOTAL_PTS <- 50000
+TOTAL_PTS <- 30000
 
 # Pull a number of points from each country proportional to the number of WOCAT
 # observations from that country. To do this need a count of WOCAT obs per
@@ -31,12 +31,16 @@ TOTAL_PTS <- 50000
 d <- read.csv(file.path(data_folder, 'wocat_database_trendsearth_indicators_20180523_clean.csv'))
 d <- SpatialPointsDataFrame(cbind(d$lon, d$lat), d, proj4string=CRS(proj4string(r)))
 
+names(d@data)
+
+d@data <- select(d@data, -lpd_te7cl_v2, -starts_with('te'))
+
 # Pull ISOs for the WOCAT data
 adm <- readOGR(data_folder, 'ne_50m_admin_0_countries_split')
 adm$id <- sequence(nrow(adm))
 d_codes <- over(d, adm[, 'ADM0_A3'])
 d$iso <- d_codes$ADM0_A3
-d$treatment <- 'treatment'
+d$treatment <- TRUE
 
 # TODO: Need to make holes in the ADM layer to remove a buffer around each
 # WOCAT observation.
@@ -54,6 +58,14 @@ d$poly_id <- d_poly_ids$id
 
 # Filter out WOCAT observations that don't fall within a polygon at all
 d <- d[!is.na(d$poly_id), ]
+
+# Add covariates for matching
+e <- extract(r, d, df=TRUE)
+e <- select(e, -ID)
+stopifnot(nrow(d@data) == nrow(e))
+d@data <- cbind(d@data, e)
+
+d$iso <- droplevels(d$iso)
 
 s <- foreach (poly_id=unique(d$poly_id), .combine=rbind) %do% {
     n_obs <- sum(d$poly_id == poly_id, na.rm=TRUE)
@@ -75,10 +87,13 @@ s <- foreach (poly_id=unique(d$poly_id), .combine=rbind) %do% {
         s <- s[in_pts, ]
     }
     s$iso <- poly$ADM0_A3
-    s$treatment <- 'control'
+    s$treatment <- FALSE
     return(as.data.frame(s))
 }
 
+s_filtered <- select(s, treatment, x, y, lpd_te7cl_v2,
+                     iso, te_lulc, te_eleva, te_slop,
+                     te_te_clima, te_ppt, te_acces, te_pop)
 
 ###############################################################################
 # Perform matching
@@ -86,15 +101,43 @@ s <- foreach (poly_id=unique(d$poly_id), .combine=rbind) %do% {
 # TODO: Filter by date as well
 
 d_land_deg <- filter(d@data, (p01_imprprod == 1) | (p02_redldegr == 1)) %>%
-  select(treatment, iso, x=lon, y=lat, lpd_te7cl_v2, te_eleva) %>%
-  bind_rows(s)
-d_land_deg <- SpatialPointsDataFrame(cbind(d_land_deg$x, d_land_deg$y),
+    select(treatment, x=lon, y=lat, lpd_te7cl_v2,
+           iso, te_lulc, te_eleva, te_slop,
+           te_te_clima, te_ppt, te_acces, te_pop) %>%
+    bind_rows(s_filtered) %>%
+    filter(complete.cases(.), lpd_te7cl_v2 >= -3)
+d_land_deg$lpd_te7cl_v2 <- factor(d_land_deg$lpd_te7cl_v2)
+
+table(d_land_deg$treatment)
+
+m <- matchit(treatment ~ iso, te_lulc, te_eleva, te_slop,
+                         te_te_clima, te_ppt, te_acces, te_pop,
+             method = "optimal")
+ggplot(match.data(m)) +
+    geom_histogram(aes(lpd_te7cl_v2, ..count../sum(..count..),
+                       fill=lpd_te7cl_v2), stat='count') +
+    facet_grid(treatment~.) +
+    xlab('Land productivity') +
+    ylab('Frequency') +
+    scale_fill_discrete() +
+    guides(fill=FALSE)
+                 
+
+
+
+
+# TODO: Temporarily reverse coords until geoMatch is fixed:
+d_land_deg_sp <- SpatialPointsDataFrame(cbind(d_land_deg$y, d_land_deg$x),
                                      d_land_deg,
                                      proj4string=CRS('+init=epsg:4326'))
+
+#match <- geoMatch(treatment ~ iso + te_eleva + te_slope + te_clima +
+#                  te_acces + te_rainf + te_lulc + te_pop
+
 
 match <- geoMatch(treatment ~ iso + te_eleva,
                   method = "nearest", 
                   caliper=0.25, 
-                  data = d_land_deg, 
+                  data = d_land_deg_sp, 
                   outcome.variable="lpd_te7cl_v2", 
                   outcome.suffix="_adjusted")
